2c132ab51da8b20a2302121b9cadc8dfa5d0046a
NaSuRall <romain.poulain@edu.esiee-it.fr>
2026-09-08 10:24:41 +0200

    SETUP ROUTER on project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Login from "./features/auth/Login.tsx";
+import Register from "./features/auth/Register.tsx";
 
 function App() {
   return (
-    <>
-      <h1>Test</h1>
-    </>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
   )
 }
 
